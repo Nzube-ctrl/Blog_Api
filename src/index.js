154cf82c13f7e6be2cdbc6aac9cb0934a191da0b
Nzube Uwakwe <nzubeuwakwe@gmail.com>
2024-04-22 19:31:49 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 //Middlewares
+app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api/user", authRoute);
 app.use("/api", blogRoute);
