b0a92588dd36330ac94cae8a6b27134fc038a40a
Nzube Uwakwe <nzubeuwakwe@gmail.com>
2024-05-07 21:41:38 +0300

    Added userValidator

---
+++ src/middlewares/userValidator.js
@@ -0,0 +1,46 @@
+const joi = require("joi");
+
+const userValidator = joi.object({
+  first_name: joi.string().min(5).max(30),
+  last_name: joi.string().min(5).max(30),
+  email: joi
+    .string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+});
+
+const userValidatorMiddleware = async (req, res, next) => {
+  const userPayload = req.body;
+  try {
+    await userValidator.validateAsync(userPayload);
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(406).send(error.details[0].message);
+  }
+};
+
+const loginValidator = joi.object({
+  email: joi
+    .string()
+    .email({ maxDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+});
+
+const loginValidatorMiddleware = async (req, res, next) => {
+  const userPayload = req.body;
+  try {
+    await loginValidator.validateAsync(userPayload);
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(406).send(error.details[0].message);
+  }
+};
+
+module.exports = {
+  userValidatorMiddleware,
+  loginValidatorMiddleware,
+};
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,17 +2,18 @@ const express = require("express");
 const authController = require("../controllers/authController.js");
 const authenticate = require("../middlewares/authentication.js");
 const logger = require("../utils/logger.js");
-
+const {
+  loginValidatorMiddleware,
+  userValidatorMiddleware,
+} = require("../middlewares/userValidator.js");
 const authRoute = express.Router();
 
-authRoute.post("/signup", (req, res) => {
-  logger.info("POST /signup requested.");
-  authController.signUp(req, res);
+authRoute.post("/signup", userValidatorMiddleware, (req, res) => {
+  logger.info("POST /signup requested."), authController.signUp(req, res);
 });
 
-authRoute.post("/signin", (req, res) => {
-  logger.info("POST /signin requested.");
-  authController.signIn(req, res);
+authRoute.post("/login", loginValidatorMiddleware, (req, res) => {
+  logger.info("POST /login requested."), authController.signIn(req, res);
 });
 
 authRoute.get("/blogs", authenticate, (req, res) => {
